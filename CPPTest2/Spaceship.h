fe42f7a1ddc49c026c04ae5f044ffc7a346a4c30
ilfinkel <ilfinkel@gmail.com>
2017-11-09 21:44:23 +0300

    добавил генерацию вселенной, пока очень простую.

--- CPPTest2/Spaceship.h
+++ CPPTest2/Spaceship.h
@@ -9,13 +9,7 @@ public:
 	Spaceship(int _placeCount, int _mass, int _driverCount, int _gunPoints, int _shipClass) { placeCount = _placeCount; mass = _mass; driverCount = _driverCount; gunPoints = _gunPoints; shipClass = _shipClass; }
 	~Spaceship();
 
-	Spaceship(Spaceship &other) {
-		this->placeCount = other.driverCount;
-		this->mass = other.mass;
-		this->driverCount = other.driverCount;
-		this->gunPoints = other.gunPoints;
-		this->shipClass = other.shipClass;
-	}
+	Spaceship(Spaceship &other) { this->placeCount = other.driverCount; this->mass = other.mass; this->driverCount = other.driverCount; this->gunPoints = other.gunPoints; this->shipClass = other.shipClass; }
 private:
 	int placeCount;		// сколько в корабль вообще помещается существ
 	int mass;
